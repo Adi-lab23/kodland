32e2b507fdb98fd4b73b0f2771d8983122b3cda7
Adi-lab23 <osadowska0@gmail.com>
2024-09-03 19:30:36 +0200

    Create main.py
    mój pirwszy kod

---
+++ main.py
@@ -0,0 +1,10 @@
+slowa = {
+    'LOL' : 'odpowiedź na coś zabawnego',
+    'CRINGE' : 'coś dziwnego lub wstydliwego',
+    'ROFL' : 'odpowiedź na żart',
+    'SHEESH' : 'lekka dezaprobata'
+    'CREEPY' : 'straszny, złowieszczy',
+    'AGGRO' : 'stać się agresywnym/zły'
+}
+a = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!):")
+print(slowa[a])
